570241eb01721c7961a9606a82c234d1dfe52642
ortizk <ortizk@live.com>
2018-03-20 18:49:58 -0700

    Commit 9 - Created loops.js file

---
+++ Unit1/Week_One/w1d2_homework/resume_page/loops.js
@@ -0,0 +1,24 @@
+for(let num = 0; num <= 99; num++) {
+	console.log(num)
+}
+
+for(let num = 99; num >= 0; num--){
+	console.log(num)
+}
+for(let num = 0; num <= 98; num=num+2) {
+	console.log(num)
+}
+for(let num = 98; num >= 0; num=num-2) {
+	console.log(num)
+}
+for(let num = 49; num <= 72; num++) {
+	console.log(num)
+}
+for(let num = 81; num >= 26; num--) {
+	console.log(num)
+}
+for(let num = 3; num <= 90; num++) {
+	if(num % 3 === 0) {
+	console.log(num)
+}
+}
