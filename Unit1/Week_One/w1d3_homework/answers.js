ea47cf94de0e8618b1ae8c6ce601ad8d03dae20f
ortizk <ortizk@live.com>
2018-03-21 09:33:09 -0700

    Commit 3 - excited kitten answered (not yet)

--- Unit1/Week_One/w1d3_homework/answers.js
+++ Unit1/Week_One/w1d3_homework/answers.js
@@ -1,6 +1,13 @@
-// for(let num = 1; num <= 20; num++) {
-// 	console.log(num)
-// }
+for(let num = 1; num <= 20; num++) {
+	console.log(num)
+}
 for(let num = 0; num <= 200; num=num+2) {
 	console.log(num)
 }
+for(let i = 0; i < 20; i++) {
+	if ( i % 2 !== 0);
+	console.log(`${i}Love me, pet me, HHHSSSSSSS!`)
+	if ( i % 2 === 0);
+	console.log(`${i} The catnip made me do it`)
+}
+// will come back to this
